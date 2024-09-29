4f69119e2b885f3e6e38181ef7b00172832dcf74
Aishwarya Raghavan <aishwarya.raghavan.785@gmail.com>
2024-09-28 22:11:13 -0700

    Made agent less verbose and added to source

--- src/agent.py
+++ src/agent.py
@@ -3,5 +3,5 @@ from langchain.agents import create_openai_tools_agent, AgentExecutor
 
 def create_agent(llm, tools, prompt):
     agent = create_openai_tools_agent(llm, tools, prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, return_intermediate_steps=False, verbose = False)
     return agent_executor
